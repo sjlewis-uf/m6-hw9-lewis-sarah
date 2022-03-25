257dc3a3b0e7764bb10bc731c96bd3814c93493f
Sarah Lewis <sj.lewis@ufl.edu>
2022-03-25 19:45:16 -0400

    HTML built, Javascript started, and CSS started

---
+++ index.js
@@ -0,0 +1,30 @@
+//get #locationi and button elements
+var weather = document.getElementByID('weather')
+var form = document.querySelector('form')
+
+//attach fetch call to button element on click
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  console.log(locationInput.value)
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?appid=7999648ce6c9798f64a2b1ee96745b27&units=imperial&q=' + locationInput.value)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(data) {
+    console.log(data)
+    var div = document.createElement('div')
+    var h2 = document.createElement('h2')
+    h2.textContent = data.city_name + ", " + data.state_code
+
+    div.appendChild(h2)
+    location.appendChild(div)
+
+
+  })
+})
+
+
+
+main.temp
+main.feels_like
